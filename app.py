5db46548fe79992a0fd8e1366b35d79221a4112c
Om Sarulkar <osarulkar@gmail.com>
2023-05-19 13:16:58 +0530

    Update app.py
    Updating app.py to include error handling methods for bypassing SSL certificates

--- app.py
+++ app.py
@@ -9,20 +9,31 @@ def scrape_reviews(url):
     HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'}
 
     def getdata(url):
-        r = requests.get(url, headers=HEADERS)
-        return r.text
+        try:
+            r = requests.get(url, headers=HEADERS, verify=False)
+            r.raise_for_status()  # Raise an exception if the response status is not successful
+            return r.text
+        except (requests.exceptions.RequestException, requests.exceptions.HTTPError) as e:
+            print(f"Error occurred during request: {e}")
+            return None
 
     def html_code(url):
         htmldata = getdata(url)
-        soup = BeautifulSoup(htmldata, 'html.parser')
-        return soup
+        if htmldata:
+            soup = BeautifulSoup(htmldata, 'html.parser')
+            return soup
+        else:
+            return None
 
     soup = html_code(url)
 
     def cus_rev(soup):
+        if soup is None:
+            return []
+
         data_str = ""
 
-        for item in soup.find_all("a", class_="a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold"):
+        for item in soup.find_all("a", class_= "a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold"):
             data_str = data_str + item.get_text()
 
         result = data_str.split("\n")
@@ -59,8 +70,5 @@ def classify():
     return render_template('index.html', reviews=reviews, predictions=predictions)
 
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
